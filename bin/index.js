86cfb28473d4d94620b97b5d2053acf4b5de0dcf
Michael Cook <mcook0775@gmail.com>
2020-02-11 22:32:55 -0500

    Minor change

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,16 @@
+#!/usr/bin/env node
+
 /* generate basics files and directories for a bootstrap project, write boilerplate 
 to those files. Good to quickly start up a little bootstrap font-end project or demo 
 an idea */
-
 
 const { makeDir } = require('../src/functions');
 const { writeFile } = require('../src/functions');
 const { dirs } = require('../data/data');
 const { content } = require('../data/data');
 
-
 // write dirs
 dirs.forEach(dir => makeDir(dir));
-
 
 // write file contents 
 for (let file in content) {
